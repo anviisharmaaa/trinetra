6b285cf9550de79897b1fdd6b028496bd441bc51
anviisharmaa <anviisharmaa@hotmail.com>
2026-09-08 21:22:40 +0530

    Push backend and frontend without gitignore

--- src/layouts/AuthLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-export function AuthLayout() {
-  return (
-    <div style={{ height: '100vh', width: '100vw', overflow: 'hidden', background: 'var(--bg-0)' }}>
-      <Outlet />
-    </div>
-  );
-}
